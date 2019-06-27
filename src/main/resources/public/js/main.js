cc1213a10d6da4ddf450af2319d1bb7dd1eb20ad
Oleksii Morenets <oleksii.morenets@gmail.com>
2019-06-27 13:40:56 +0300

    Service -> output some static list fixed main.js

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,9 +1,10 @@
 var app = angular.module("springDemo", []);
 
-app.controller("AppCtrl", function($scope, $http) {
+app.controller("AppCtrl", function ($scope, $http) {
     $scope.websites = [];
 
-    $http.get('http://localhost:8090/api/stackoverflow').success(function(data) {
-        $scope.websites = data;
-    });
+    $http.get('http://localhost:8090/api/stackoverflow')
+        .then(function (response) {
+            $scope.websites = response.data;
+        });
 });
